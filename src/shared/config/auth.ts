d7e6ff847bfd65525866f9470d6eaea13f9fd844
송재욱 <haensol976520@gmail.com>
2025-03-07 15:13:20 +0900

    fix: auth config

--- src/shared/config/auth.ts
+++ src/shared/config/auth.ts
@@ -18,15 +18,24 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
       }
       return token;
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken;
+      if (token) {
+        session.accessToken = token.accessToken;
+      }
       return session;
     },
   },
   pages: {
     signIn: "/login",
+    signOut: "/login",
   },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
